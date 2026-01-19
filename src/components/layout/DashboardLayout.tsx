eda70290f02402c0e2192b1605359d4c707e7251
aliakborswe <aliakbor.js@gmail.com>
2026-01-19 15:56:25 +0600

    add dropdown theming

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -6,7 +6,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { Outlet } from "react-router";
-import DarkLight from "./DarkLight";
+import { ModeToggle } from "./mode-toggle";
 
 export default function DashboardLayout() {
   return (
@@ -14,14 +14,14 @@ export default function DashboardLayout() {
       <AppSidebar />
       <SidebarInset>
         <header className='flex h-16 shrink-0 justify-between items-center gap-2 border-b px-4 '>
-          <div className="flex items-center gap-1">
+          <div className='flex items-center gap-1'>
             <SidebarTrigger className='-ml-1' />
             <Separator
               orientation='vertical'
               className='mr-2 data-[orientation=vertical]:h-4'
             />
           </div>
-          <DarkLight />
+          <ModeToggle />
         </header>
         <div className='flex flex-1 flex-col gap-4 p-4'>
           <Outlet />
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,7 +5,8 @@ import ActiveLink from "./ActiveLink";
 import { useProfileQuery } from "@/redux/features/auth/auth.api";
 import Logo from "./Logo";
 import Logout from "./Logout";
-import DarkLight from "./DarkLight";
+
+import { ModeToggle } from "./mode-toggle";
 
 export default function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -39,20 +40,20 @@ export default function Navbar() {
       <div className='container mx-auto flex h-16 items-center justify-between px-4'>
         <Logo />
         <div onClick={toggleMenu} className='lg:hidden flex items-center gap-3'>
-          <DarkLight />
+          <ModeToggle />
           {showMenu ? <X size={36} /> : <Menu size={36} />}
         </div>
 
         <div
-          ref={menuRef}
+          // ref={menuRef}
           className={`${
             showMenu ? "block" : "hidden"
           } lg:block absolute w-[97%] lg:w-fit lg:static top-[62px] left-2.5 p-4 rounded-xl bg-background lg:bg-transparent border lg:border-none shadow-md lg:shadow-none`}
         >
           <div className='lg:flex justify-between items-center'>
-            <div className='flex flex-col lg:flex-row gap-6 text-base font-medium text-foreground'>
+            <div className='flex flex-col lg:flex-row items-center gap-6 text-base font-medium text-foreground'>
               <div className='hidden lg:block'>
-                <DarkLight />
+                <ModeToggle />
               </div>
               <ActiveLink to='/'>Home</ActiveLink>
               <ActiveLink to='/about'>About</ActiveLink>
